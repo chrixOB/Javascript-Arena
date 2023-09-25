7db2f4198800d0408e239827d247f485ce57bf22
chrixob <chrisobodai40@gmail.com>
2023-09-25 21:30:35 +0000

    Update arrow.js

--- arrow.js
+++ arrow.js
@@ -1,5 +1,14 @@
 // lets  explore about arrow functions
-
+// let l = prompt("enter length","")
+// let w = prompt("enter width","")
 let Area = (l,w) => l * w;
+const num1 = 20;
+console.log("area is " + Area(10,2));
 
-console.log(Area(4,5));
+if(Area <= num1){
+    console.log("less or equal to 20 ");
+}
+else
+{
+    console.log("greater than 20");
+}
