d071665ab0d8d770caea7fb6866c15f7a384a694
chrixob <chrisobodai40@gmail.com>
2023-09-29 21:56:09 +0000

    styled with bootstrap

--- reviews-project/reviews.js
+++ reviews-project/reviews.js
@@ -1,37 +1,136 @@
+// const reviews = [
+//     {
+//         id: 0,
+//         name: 'Christopher smith',
+//         job: 'frontend developer',
+//         img: './review_images/chris.jpg',
+//         // img: 'C:\Users\HP ELITEBOOK\Documents\GitHub\Javascript-Arena\reviews-project\review_images\chris.jpg',
+//         information: 'qwerty ui op asdf ghjk l zxc v bnm ssjs siydi iuedied i k oio game f;iauhf dsuifad;ifu asufdiud uasdh;dufdf uidh;difdf isdhodfh oidhodifd oihdfosidhf oidhods uiaukasjbcjbcjwb kuaskuaskusaaso  jyfjy grsyersyer fhtrsdhtr fore osuhduif sdufgisdfgisd onoli qwerty asd gfjin xmzknl zkjckxjc  kzjckj'
+//     },
+//     {
+//         id: 1,
+//         name: 'Devi Vishwakuma',
+//         job: 'backend developer',
+//         img: './review_images/devi.png',
+//         information: 'I am a backend developer and it feels so graet. I work with node.js, express.js, php, python, mongoDB, MySQL and PostgreSQL. My duties involves working with servers and interacting with databases. I work behind the scenes, hand in hand with the frontend developer to create and maintain applications'
+//     },
+//     {
+//         id: 2,
+//         name: 'James Rodriguez',
+//         job: 'UX designer',
+//         img: './review_images/chris1.jpg',
+//         information: 'rambo race qwerty ui op asdf ghjk l zxc v bnm ssjs siydi iuedied i k oio game uiaukasjbcjbcjwb kuaskuaskusaaso  jyfjy grsyersyer fhtrsdhtr fore aksjalsdhals idhslaidalsdlsh lslasihdlsai asahlsdihas laisdlasi liahdilasj alihlaish laishlaihs aisj'
+//     },
+//     {
+//         id: 3,
+//         name: 'Jim Parkson',
+//         job: 'mobile developer',
+//         img: './review_images/cybernet.jpg',
+//         information: 'obolo ui qwerty ui op asdf ghjk l zxc v bnm skjaskas kajsnlasdlas alkndlslaskdssjs siydi iuedied i k oio game uiaukasjbcjbcjwb kuaskuaskusaaso  jyfjy grsyersyer fhtrsdhtr foreaksjnskjdnas kajskasjnda jksajsljas aksjulashdalsd alsjhlasihlasdas'
+//     },
+//     {
+//         id: 4,
+//         name: 'Nessy Adams',
+//         job: 'intern',
+//         img: './review_images/wednesday.png',
+//         information: 'banana pie qwerty ui op asdf ghjk l zxc v bnm ssjs siydi iuedied i k oio game uiaukasjbcjbcjwb kuaskuaskusaaso  jyfjy grsyersyer fhtrsdhtr fore'
+//     },
+// ]
+
+// const img = document.getElementById("image")
+// const fullname = document.getElementById("name")
+// const job = document.getElementById("job-description")
+// const info = document.querySelector(".info")
+
+// // targeting the buttons
+// const prevBtn = document.querySelector(".prev-btn")
+// const nxtBtn = document.querySelector(".next-btn")
+// const randomBtn = document.querySelector(".random-btn")
+
+// currentItem = 0;
+
+// // load Initial item
+// window.addEventListener("DOMContentLoaded", () => {
+//     const item = reviews[currentItem];
+//     img.src = item.img;
+//     fullname.textContent = item.name;
+//     job.textContent = item.job;
+//     info.textContent = item.information;
+// });
+
+// //show person baesd on item index
+// function showPerson(currentItem){
+//     const item = reviews[currentItem]
+//     img.src = item.img;
+//     fullname.textContent = item.name;
+//     job.textContent = item.job;
+//     info.textContent = item.information;
+// }
+
+// nxtBtn.addEventListener("click", () => {
+//     currentItem++;
+//     console.log(currentItem);
+//     if(currentItem > reviews.length - 1){
+
+//         currentItem = 0;
+//     }
+//     showPerson(currentItem);
+// })
+
+// prevBtn.addEventListener("click", () => {
+//     currentItem--;
+//     console.log(currentItem);
+//     if(currentItem < 0){
+
+//         currentItem = reviews.length - 1;
+//     }
+//     showPerson(currentItem);
+// })
+
+// randomBtn.addEventListener("click", () => {
+//     let randomNumber = Math.floor(Math.random() * reviews.length);
+//     console.log(randomNumber)
+//     showPerson(randomNumber);
+// })
+
+
+
+
+
 const reviews = [
     {
         id: 0,
         name: 'Christopher smith',
-        job: 'frontend developer',
+        job: 'Frontend Developer',
         img: './review_images/chris.jpg',
         // img: 'C:\Users\HP ELITEBOOK\Documents\GitHub\Javascript-Arena\reviews-project\review_images\chris.jpg',
-        information: 'qwerty ui op asdf ghjk l zxc v bnm ssjs siydi iuedied i k oio game uiaukasjbcjbcjwb kuaskuaskusaaso  jyfjy grsyersyer fhtrsdhtr fore'
+        information: 'qwerty ui op asdf ghjk l zxc v bnm ssjs siydi iuedied i k oio game f;iauhf dsuifad;ifu asufdiud uasdh;dufdf uidh;difdf isdhcjwb '
     },
     {
         id: 1,
         name: 'Devi Vishwakuma',
-        job: 'backend developer',
+        job: 'Backend Developer',
         img: './review_images/devi.png',
-        information: 'asdf qwerty ui op asdf ghjk l zxc v bnm ssjs siydi iuedied i k oio game uiaukasjbcjbcjwb kuaskuaskusaaso  jyfjy grsyersyer fhtrsdhtr fore'
+        information: 'I am a backend developer and it feels so graet. I work with node.js, express.js, php, python, mongoDB, MySQL and PostgreSQL. My duties involves working with servers and interacting with databases.'
     },
     {
         id: 2,
         name: 'James Rodriguez',
-        job: 'UX designer',
+        job: 'UX Designer',
         img: './review_images/chris1.jpg',
-        information: 'rambo race qwerty ui op asdf ghjk l zxc v bnm ssjs siydi iuedied i k oio game uiaukasjbcjbcjwb kuaskuaskusaaso  jyfjy grsyersyer fhtrsdhtr fore'
+        information: 'rambo race qwerty ui op asdf ghjk l zxc v bnm ssjs siydi iuedied i k oio game uiaukasjbcjbcjwb kuaskuaskusaaso  jyfjy grsyersyer fhtrsdhtr fore aksjals'
     },
     {
         id: 3,
         name: 'Jim Parkson',
-        job: 'mobile developer',
+        job: 'Mobile Developer',
         img: './review_images/cybernet.jpg',
-        information: 'obolo ui qwerty ui op asdf ghjk l zxc v bnm ssjs siydi iuedied i k oio game uiaukasjbcjbcjwb kuaskuaskusaaso  jyfjy grsyersyer fhtrsdhtr fore'
+        information: 'obolo ui qwerty ui op asdf ghjk l zxc v bnm skjaskas kajsnlasdlas alkndlslaskdssjs siydi iuedied i k oio game uiaukasjbcjbcjwb kuaskuaskusaaso  jyfjy grsyersyer fhtrs.'
     },
     {
         id: 4,
         name: 'Nessy Adams',
-        job: 'intern',
+        job: 'Intern',
         img: './review_images/wednesday.png',
         information: 'banana pie qwerty ui op asdf ghjk l zxc v bnm ssjs siydi iuedied i k oio game uiaukasjbcjbcjwb kuaskuaskusaaso  jyfjy grsyersyer fhtrsdhtr fore'
     },
@@ -88,7 +187,7 @@ prevBtn.addEventListener("click", () => {
 })
 
 randomBtn.addEventListener("click", () => {
-    let randomNumber = Math.random;
+    let randomNumber = Math.floor(Math.random() * reviews.length);
     console.log(randomNumber)
-    // showPerson();
+    showPerson(randomNumber);
 })
