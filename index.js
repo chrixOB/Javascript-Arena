c15abd412f8fcaa7fd7672d08bdef21ec7ebeefb
chrixob <chrisobodai40@gmail.com>
2023-11-11 02:05:13 +0000

    error fixes

---
+++ index.js
@@ -0,0 +1,21 @@
+let fruits = ["orange", "apple", "pear", "coconut"];
+let veggies = ["luttuce", "carrot", "onion"];
+
+let both = [...fruits,"qwerty", "azerty"];
+console.log(both)
+
+negative = () =>  {
+    console.log("age is not 5")
+  
+}
+
+affirmative = () => console.log("age is 5");
+
+ const age = 5;
+ age!=5 
+ ?
+ negative()
+ :
+ affirmative()
+ 
+ 
